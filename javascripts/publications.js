2ccdcaeb2a671e6917e347b65b28fc9ce057d080
Sebastien Cadorette <cado.94@me.com>
2019-10-09 22:18:22 -0400

    Publications function for all the javascript

--- javascripts/publications.js
+++ javascripts/publications.js
@@ -2,6 +2,19 @@
 /* jQuery selector for the publications navigation link in the header */
 const publicationsLinkSelector = '#publications-link';
 
+const Publications = () => {
+
+	function setNavBarActiveButton () {
+		$(publicationsLinkSelector).addClass('active');
+	}
+
+	return {
+		init: () => {
+			setNavBarActiveButton();
+		}
+	}
+};
+
 $(document).ready(() => {
-	$(publicationsLinkSelector).addClass('active');
+	Publications().init();
 });
