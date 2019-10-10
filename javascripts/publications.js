f71784e274dea78ba57d0a1a893f4903146b191d
Sebastien Cadorette <cado.94@me.com>
2019-10-09 22:59:00 -0400

    Publications Filter by query string

--- javascripts/publications.js
+++ javascripts/publications.js
@@ -17,10 +17,31 @@ const Publications = () => {
 		});
 	}
 
+	// https://stackoverflow.com/questions/5999118/how-can-i-add-or-update-a-query-string-parameter
+	function _updateQueryStringParameter(uri, key, value) {
+		const re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+		const separator = uri.indexOf('?') !== -1 ? "&" : "?";
+		if (uri.match(re)) {
+			return uri.replace(re, '$1' + key + "=" + value + '$2');
+		}
+		else {
+			return uri + separator + key + "=" + value;
+		}
+	}
+
+	function handleFilterBy () {
+		$('#fieldFilterSection').on('change', (event) => {
+			const newValue = $( "#fieldFilterSection option:selected" ).text()
+			const newUrl = _updateQueryStringParameter(window.location.href, 'sort_by', newValue);
+			window.location.href = newUrl;
+		});
+	}
+
 	return {
 		init: () => {
 			setNavBarActiveButton();
 			handleDeleteIconButton();
+			handleFilterBy();
 		}
 	}
 };
