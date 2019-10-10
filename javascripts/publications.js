7ed49f320fdb6b21dfa47a61c4a0950a4a1e16a9
Sebastien Cadorette <cado.94@me.com>
2019-10-09 22:31:07 -0400

    Publications delete icon in a selector variable

--- javascripts/publications.js
+++ javascripts/publications.js
@@ -2,6 +2,9 @@
 /* jQuery selector for the publications navigation link in the header */
 const publicationsLinkSelector = '#publications-link';
 
+/* jQuery selector for the delete icon in the publications table */
+const deleteIconSelector = '.del-icon';
+
 const Publications = () => {
 
 	function setNavBarActiveButton () {
@@ -9,7 +12,7 @@ const Publications = () => {
 	}
 
 	function handleDeleteIconButton () {
-		$('.del-icon').click((event) => {
+		$(deleteIconSelector).click((event) => {
 			$(event.target).closest('tr').remove();
 		});
 	}
